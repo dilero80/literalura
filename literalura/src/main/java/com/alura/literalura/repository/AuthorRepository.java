aa39401e470acdcf251f0fd361dabe0043d5b087
Diego Romero <dilero@gmail.com>
2024-05-16 16:00:01 -0500

    ReadyMinRequirements

---
+++ literalura/src/main/java/com/alura/literalura/repository/AuthorRepository.java
@@ -0,0 +1,9 @@
+package com.alura.literalura.repository;
+
+import com.alura.literalura.entities.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author,Long> {
+}
